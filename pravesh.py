a4dad4c5c83491358f5a88f3cf5a2726b8d2bad0
Kartik Kannapur <kartikkannapur@gmail.com>
2014-12-25 00:56:21 +0530

    Index.html Created

--- pravesh.py
+++ pravesh.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def start():
-    return 'Hello World!'
+def index():
+	return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+	app.debug = True
+	app.run()
