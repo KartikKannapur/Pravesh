cb632f00125fd15d2bfb2362fc2cd477462cd249
Anuj Duggal <anuj.duggal@intel.com>
2015-01-10 00:11:19 +0530

    Added Create Event Service.
    Modified parveshSqlConnect.py and praveshService.py for including Create Event Service.
    
    Signed-off-by: Anuj Duggal <anuj.duggal@intel.com>

--- praveshServices.py
+++ praveshServices.py
@@ -1,7 +1,7 @@
 #
 #                       AUTHOR                  :                       ANUJ DUGGAL
 #                       DATE CREATED            :                       DECEMBER 31, 2014
-#                       DATE MODIFIED           :                       DECEMBER 31, 2014
+#                       DATE MODIFIED           :                       JANUARY 9, 2015
 #                       VERSION                 :                       1.0
 #                       DESCRIPTION             :                       Services exposed as APIs for Pravesh functionality
 #
@@ -30,6 +30,61 @@ class pravesh:
                 # FREE THE OBJECT AND CLOSE THE CONNECTION
                 o_praveshSqlConnect.free()
 
+        # TODO: POST EVENT 
+        # 1. UPDATE tbl_event --> WHO ALL ATTENDED THE EVENT
+        # 2. UPDATE tbl_developerPoints --> ASSIGN POINTS TO DEVELOPER FOR AN EVENT (ON SOME TECHNOLOGY)
+        # 3. UPDATE tbl_developer --> UPDATE event_id field (APPEND event_id to already existing event_id's)
+
+	#TODO:  ------ PRE-EVENT TASKS ------:
+
+	# CREATE NEW EVENT:
+	def createEvent(self, name, event_date, venue, event_type, description, technology_id, googlePlus_link, event_points):
+		print "Creating New Event..."
+		o_praveshSqlConnect = praveshSqlConnect()
+		
+		# PUSH EVENT DETAILS TO DATABASE:
+		o_praveshSqlConnect.createEvent(name, event_date, venue, event_type, description, technology_id, googlePlus_link, event_points)
+
+		o_praveshSqlConnect.free()
+
+
+	def updateEventDetails(self, event_id, name, event_date, venue, event_type, description, technology_id, googlePlus_link, event_points):
+                o_praveshSqlConnect = praveshSqlConnect()
+
+		# UPDATE ALREADY EXISTING EVENT INFORMATION:
+                o_praveshSqlConnect.updateEventDetails(event_id, name, event_date, venue, event_type, description, technology_id, googlePlus_link, event_points)
+
+                o_praveshSqlConnect.free()
+
+
+	def signUpDeveloperForEvent():
+		# WE NEED TO ASSIGN SOME IDENTITY/QR CODE, ETC.. WHICH WILL BE SCANNED DURING THE EVENT
+                o_praveshSqlConnect = praveshSqlConnect()
+
+                o_praveshSqlConnect.signUpDeveloperForEvent()
+
+                o_praveshSqlConnect.free()
+
+
+	# TODO: ------ DURING EVENT TASKS -------:
+	def developerAttended():
+                o_praveshSqlConnect = praveshSqlConnect()
+
+		o_praveshSqlConnect.whoAttendedEvent()
+                o_praveshSqlConnect.assignPoints()
+		o_praveshSqlConnect.updateDeveloperProfile()
+
+                o_praveshSqlConnect.free()
+
+
+
+
+
+	# TODO: ------ POST EVENT TASKS ------:
+
+
+
+
 # TODO: 
 # 1. LINK THE BELOW FUNCTIONALITY WITH FRON END (REGISTRATYION FORM) - THE BELOW CODE IS ONLY FOR TESTING 
 
@@ -38,3 +93,9 @@ class pravesh:
 
 # CREATING DEVELOPER PROFILE IN PRAVESH PORTAL:
 #o_pravesh.createDeveloperProfile("Anuj", "Duggal", "er.anujduggal@gmail.com", "9980962767", "21/12/1988", "male", "Intel", "anujduggal88", "anujduggal21", "", "Polymer, Android")
+
+o_pravesh = pravesh()
+#o_pravesh.createEvent("TestName", "Test Date", "Test Venue", "Meetup", "Test Desc", "Android, Chrome", "Test Link", 2)
+
+#o_pravesh.updateEventDetails()
+
--- praveshSqlConnect.py
+++ praveshSqlConnect.py
@@ -1,7 +1,7 @@
 #
 #                       AUTHOR                  :                       ANUJ DUGGAL
 #                       DATE CREATED            :                       DECEMBER 31, 2014
-#                       DATE MODIFIED           :                       DECEMMBER 31, 2014
+#                       DATE MODIFIED           :                       JANUARY 9, 2015
 #                       VERSION                 :                       1.0
 #                       DESCRIPTION             :                       DATABASE CONNECTION, SQL QUERIES
 #
@@ -27,10 +27,45 @@ class praveshSqlConnect:
         if self.conn:
             self.conn.close()
 
+    # SIGNUP DEVELOPER FOR THE FIRST TIME:
     def pushDeveloperDetails(self, fname, lname, email, phone, dob, sex, organization, github, linkedin, googleplus, technology):
-	print "[TESTING]: Pushing Developer Details to Database..."
-	self.cur.execute("INSERT INTO tbl_developer(firstname, lastname, email, phone, dateOfBirth, sex, organization, github_link, linkedin_link, googlePlus_link, technology_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", (fname, lname, email, phone, dob, sex, organization, github, linkedin, googleplus, technology))
-	self.conn.commit()
-	print "[TESTING]: Pushed the details."
 
+	self.cur.execute("INSERT INTO tbl_developer(firstname, lastname, email, phone, dateOfBirth, sex, organization, github_link, linkedin_link, googlePlus_link, technology_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);", (fname, lname, email, phone, dob, sex, organization, github, linkedin, googleplus, technology))
+
+	self.conn.commit()
+
+    # CREATE A NEW EVENT:
+    def createEvent(self, name, event_date, venue, event_type, description, technology_id, googlePlus_link, event_points):
+
+	print "[TESTING]: CREATING NEW EVENT..."
+	self.cur.execute("INSERT INTO tbl_event(name, event_date, venue, type, description, technology_id, googlePlusLink, event_points) VALUES (%s,%s,%s,%s,%s,%s,%s,%s);", (name, event_date, venue, event_type, description, technology_id, googlePlus_link, event_points))
+
+	self.conn.commit()
+	print "[TESTING]: NEW EVENT CREATED SUCCESSFULLY!"
+
+    # UPDATE ALREADY EXISTING EVENT DETAILS (EXCEPT WHO ALL ATTENDED):
+    def updateEventDetails(self, event_id, name, event_date, venue, event_type, description, technology_id, googlePlus_link, event_points):
+
+	print "[TESTING]: UPDATING ALREADY EXISTING EVENT..."
+	self.cur.execute("UPDATE tbl_event SET venue=%s, description=%s, technology_id=%s, event_points=%s WHERE id=%s", (name, event_id))
+
+	self.conn.commit()
+	print "[TESTING]: EVENT UPDATED SUCCESSFULLY!"
+
+    # SIGNUP THE REGISTERED DEVELOPER FOR AN EVENT: UPDATE dev_registered field of tbl_event:
+    def signUpDeveloperForEvent():
+	print "Test"
+
+
+
+    # ------------------------------DURING EVENT SQL CALLS------------------------------------:
+    def whoAttendedEvent():
+	print "Test"
+
+    def assignPoints():
+	print "Test"
+
+    # UPDATE DEVELOPER PROFILE WITH EVENTS HE HAS ATTENDED: UPDATE event_id field in tbl_developer:
+    def updateDeveloperProfile():
+	print "Test"
 
